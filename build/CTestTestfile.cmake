a1d7e715aa5a34e523d95cb81503a5e3cd81f982
xgy <995975102@qq.com>
2023-01-05 18:52:39 -0800

    refine

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xgy/Desktop/aitisa_api
-# Build directory: /home/xgy/Desktop/aitisa_api/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../third_party/gtest/gtest-build")
-subdirs("test")
