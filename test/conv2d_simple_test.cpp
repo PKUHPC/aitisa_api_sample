d745ad66977ea442a750afde51957110346fc64a
dhs <7911198+wx_67030bc611@user.noreply.gitee.com>
2023-01-08 23:35:09 +0800

    add test_simple_conv2d

---
+++ test/conv2d_simple_test.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+extern "C"
+{
+#include "src/basic/factories.h"
+#include "src/nn/conv2d_simple.h"
+    // #include "src/tool/tool.h"
+}
