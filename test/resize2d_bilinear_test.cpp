a2c103eb7273a92a63257e810e54bf1133f655ac
dhs <7911198+wx_67030bc611@user.noreply.gitee.com>
2023-01-09 18:46:29 +0800

    add a case of (5,5)->(3,3) of resize2d op.

--- test/resize2d_bilinear_test.cpp
+++ test/resize2d_bilinear_test.cpp
@@ -42,5 +42,30 @@ TEST(Resize, Float2d_case1) {
   aitisa_destroy(&input);
   aitisa_destroy(&output);
 }
+
+TEST(Resize, Float2d_case2) {
+  Tensor input;
+  DataType dtype = kFloat;
+  Device device = {DEVICE_CPU, 0};
+  int64_t dims[2] = {5, 5};
+  aitisa_create(dtype, device, dims, 2, NULL, 0, &input);
+  resize2d_bilinear_assign_float(input);
+
+  Tensor output;
+  aitisa_resize2d_bilinear(input, 3, 3, &output);
+
+  float* out_data = (float*)aitisa_tensor_data(output);
+  float test_data[] = {0.,         0.43333333, 0.46666667, 0.56666667, 1.,
+                       1.03333333, 1.53333333, 1.96666667, 2.};
+  int64_t size = aitisa_tensor_size(output);
+  for (int64_t i = 0; i < size; i++) {
+    /* Due to the problem of precision, consider the two numbers
+       are equal when their difference is less than 0.000001*/
+    EXPECT_TRUE(abs(out_data[i] - test_data[i]) < 0.000001);
+  }
+
+  aitisa_destroy(&input);
+  aitisa_destroy(&output);
+}
 }  // namespace
 }  // namespace aitisa_api
