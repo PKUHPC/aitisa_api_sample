c4c4929560b5f345967d797c98914bbd1bde30b4
xgy <995975102@qq.com>
2023-01-10 08:37:50 -0800

    add matmul && resize test

--- test/matmul_simple_test.cpp
+++ test/matmul_simple_test.cpp
@@ -1,148 +1,70 @@
-#include <iostream>
-//#include "test/test_data/read_data.h"
-//extern "C" {
-//#include "src/basic/factories.h"
-//#include "src/math/matmul_simple.h"
-//}
-//
-//void matmul_simple_assign_float(Tensor t) {
-//  int64_t ndim = aitisa_tensor_ndim(t);
-//  int64_t* dims = aitisa_tensor_dims(t);
-//  int64_t size = aitisa_tensor_size(t);
-//  float* data = (float*)aitisa_tensor_data(t);
-//  float value = 0.0;
-//  for (int i = 0; i < size; ++i) {
-//    value = i * 0.1;
-//    data[i] = value;
-//  }
-//}
-//
-//void matmul_simple_assign_double(Tensor t) {
-//  int64_t ndim = aitisa_tensor_ndim(t);
-//  int64_t* dims = aitisa_tensor_dims(t);
-//  int64_t size = aitisa_tensor_size(t);
-//  double* data = (double*)aitisa_tensor_data(t);
-//  double value = 0.0;
-//  for (int i = 0; i < size; ++i) {
-//    value = i * 0.1;
-//    data[i] = value;
-//  }
-//}
-//
-//void matmul_simple_assign_int(Tensor t) {
-//  int64_t ndim = aitisa_tensor_ndim(t);
-//  int64_t* dims = aitisa_tensor_dims(t);
-//  int64_t size = aitisa_tensor_size(t);
-//  int* data = (int*)aitisa_tensor_data(t);
-//  int value = 0;
-//  for (int i = 0; i < size; ++i) {
-//    value = i;
-//    data[i] = value;
-//  }
-//}
-//
-//namespace aitisa_api {
-//namespace {
-//TEST(Matmul_float_case1, matrix_matrix) {
-//  Tensor tensor1;
-//  Tensor tensor2;
-//  DataType dtype = kFloat;
-//  Device device = {DEVICE_CPU, 0};
-//  int64_t dims1[2] = {5, 4};
-//  int64_t dims2[2] = {4, 3};
-//  aitisa_full(dtype, device, dims1, 2, 0.0, &tensor1);
-//  aitisa_full(dtype, device, dims2, 2, 0.0, &tensor2);
-//  matmul_simple_assign_float(tensor1);
-//  matmul_simple_assign_float(tensor2);
-//  Tensor output;
-//  aitisa_matmul_simple(tensor1, tensor2, &output);
-//
-//  int64_t expected_ndim = 2;
-//  int64_t expected_dims[2] = {5, 3};
-//  EXPECT_EQ(expected_ndim, aitisa_tensor_ndim(output));
-//  for (int i = 0; i < expected_ndim; ++i) {
-//    EXPECT_EQ(expected_dims[i], aitisa_tensor_dim(output, i));
-//  }
-//  auto* data = (float*)aitisa_tensor_data(output);
-//  char path[] = "../../test/test_data/Matmul_float_case1.dat";
-//  float result[15];
-//  read_date(path, result);
-//
-//  for (int i = 0; i < aitisa_tensor_size(output); ++i) {
-//    EXPECT_FLOAT_EQ(result[i], data[i]);
-//  }
-//  aitisa_destroy(&tensor1);
-//  aitisa_destroy(&tensor2);
-//  aitisa_destroy(&output);
-//}
-//
-//TEST(Matmul_double_case1, matrix_matrix) {
-//  Tensor tensor1;
-//  Tensor tensor2;
-//  DataType dtype = kDouble;
-//  Device device = {DEVICE_CPU, 0};
-//  int64_t dims1[2] = {5, 4};
-//  int64_t dims2[2] = {4, 3};
-//  aitisa_full(dtype, device, dims1, 2, 0.0, &tensor1);
-//  aitisa_full(dtype, device, dims2, 2, 0.0, &tensor2);
-//  matmul_simple_assign_double(tensor1);
-//  matmul_simple_assign_double(tensor2);
-//  Tensor output;
-//  aitisa_matmul_simple(tensor1, tensor2, &output);
-//
-//  int64_t expected_ndim = 2;
-//  int64_t expected_dims[2] = {5, 3};
-//  EXPECT_EQ(expected_ndim, aitisa_tensor_ndim(output));
-//  for (int i = 0; i < expected_ndim; ++i) {
-//    EXPECT_EQ(expected_dims[i], aitisa_tensor_dim(output, i));
-//  }
-//  auto* data = (double*)aitisa_tensor_data(output);
-//  char path[] = "../../test/test_data/Matmul_double_case1.dat";
-//  double result[15];
-//  read_date(path, result);
-//  for (int i = 0; i < aitisa_tensor_size(output); ++i) {
-//    EXPECT_DOUBLE_EQ(result[i], data[i]);
-//  }
-//  aitisa_destroy(&tensor1);
-//  aitisa_destroy(&tensor2);
-//  aitisa_destroy(&output);
-//}
-//
-//TEST(Matmul_int_case1, matrix_matrix) {
-//  Tensor tensor1;
-//  Tensor tensor2;
-//  DataType dtype = kInt32;
-//  Device device = {DEVICE_CPU, 0};
-//  int64_t dims1[2] = {5, 4};
-//  int64_t dims2[2] = {4, 3};
-//  aitisa_full(dtype, device, dims1, 2, 0, &tensor1);
-//  aitisa_full(dtype, device, dims2, 2, 0, &tensor2);
-//  matmul_simple_assign_int(tensor1);
-//  matmul_simple_assign_int(tensor2);
-//  Tensor output;
-//  aitisa_matmul_simple(tensor1, tensor2, &output);
-//
-//  int64_t expected_ndim = 2;
-//  int64_t expected_dims[2] = {5, 3};
-//  EXPECT_EQ(expected_ndim, aitisa_tensor_ndim(output));
-//  for (int i = 0; i < expected_ndim; ++i) {
-//    EXPECT_EQ(expected_dims[i], aitisa_tensor_dim(output, i));
-//  }
-//  auto data = (int*)aitisa_tensor_data(output);
-//  char path[] = "../../test/test_data/Matmul_int_case1.dat";
-//  int result[15];
-//  read_date(path, result);
-//  for (int i = 0; i < aitisa_tensor_size(output); ++i) {
-//    EXPECT_TRUE(abs(result[i] - data[i]) < 0.000001);
-//  }
-//  aitisa_destroy(&tensor1);
-//  aitisa_destroy(&tensor2);
-//  aitisa_destroy(&output);
-//}
-//
-//}  // namespace
-//}  // namespace aitisa_api
-int main(int argc, char **argv) {
-  std::cout << "matmul" << std::endl;
-  return 0;
+#include "test_utlis.h"
+extern "C" {
+#include "src/math/matmul_simple.h"
+}
+int main(int argc, char** argv) {
+  if (argc == 4) {
+    Tensor input1, input2, output;
+    Device device = {DEVICE_CPU, 0};
+    int64_t input1_ndim, input2_ndim, result_ndim, input1_dtype, input2_dtype,
+        result_dtype, input1_num, input2_num, result_num;
+    std::vector<int64_t> input1_dims_vector, input2_dims_vector,
+        result_dims_vector;
+    std::vector<float> input1_data_vector, input2_data_vector,
+        result_data_vector;
+    read_data(argv[1], &input1_ndim, &input1_dims_vector, &input1_dtype,
+              &input1_num, &input1_data_vector);
+    read_data(argv[2], &input2_ndim, &input2_dims_vector, &input2_dtype,
+              &input2_num, &input2_data_vector);
+
+    int64_t* input1_dims;
+    input1_dims = (int64_t*)malloc(sizeof(int64_t) * input1_ndim);
+    memset(input1_dims, 0, sizeof(float) * input1_ndim);
+    for (int i = 0; i < input1_ndim; i++) {
+      input1_dims[i] = input1_dims_vector[i];
+    }
+
+    int64_t* input2_dims;
+    input2_dims = (int64_t*)malloc(sizeof(int64_t) * input2_ndim);
+    memset(input2_dims, 0, sizeof(float) * input2_ndim);
+    for (int i = 0; i < input2_ndim; i++) {
+      input2_dims[i] = input2_dims_vector[i];
+    }
+
+    aitisa_create(aitisa_int_to_dtype(input1_dtype), device, input1_dims,
+                  input1_ndim, nullptr, 0, &input1);
+    aitisa_create(aitisa_int_to_dtype(input2_dtype), device, input2_dims,
+                  input2_ndim, nullptr, 0, &input2);
+    assign(input1, input1_data_vector);
+    assign(input2, input2_data_vector);
+    aitisa_matmul_simple(input1, input2, &output);
+
+    auto output_data = (float*)aitisa_tensor_data(output);
+    read_data(argv[3], &result_ndim, &result_dims_vector, &result_dtype,
+              &result_num, &result_data_vector);
+
+    int64_t output_size = aitisa_tensor_size(output);
+    for (int i = 0; i < output_size; ++i) {
+      //      std::cout << output_data[i] << std::endl;
+      if (abs(output_data[i] - result_data_vector[i]) > 1e-3) {
+        std::cout << " mismatch" << std::endl;
+        exit(1);
+      }
+    }
+    free(input1_dims);
+    free(input2_dims);
+
+    aitisa_destroy(&input1);
+    aitisa_destroy(&input2);
+    aitisa_destroy(&output);
+  } else if (argc > 4) {
+    std::cout << "Too many arguments supplied." << std::endl;
+    exit(1);
+  } else {
+    std::cout
+        << "Three argument expected. Usage: ./matmul_simple_test <input1_path> "
+           "<input2_path> <result_path> "
+        << std::endl;
+    exit(1);
+  }
 }
--- test/resize2d_bilinear_test.cpp
+++ test/resize2d_bilinear_test.cpp
@@ -1,76 +1,53 @@
-#include <iostream>
-//#include "test/test_data/read_data.h"
-//extern "C" {
-//#include "src/nn/resize2d_bilinear.h"
-//}
-//
-//void resize2d_bilinear_assign_float(Tensor t) {
-//  int64_t size = aitisa_tensor_size(t);
-//  float* data = (float*)aitisa_tensor_data(t);
-//  float value = 0;
-//  for (int i = 0; i < size; ++i) {
-//    value = i * 0.1;
-//    data[i] = value;
-//  }
-//}
-//
-//namespace aitisa_api {
-//namespace {
-//
-//TEST(Resize, Float2d_case1) {
-//  Tensor input;
-//  DataType dtype = kFloat;
-//  Device device = {DEVICE_CPU, 0};
-//  int64_t dims[2] = {3, 3};
-//  aitisa_create(dtype, device, dims, 2, NULL, 0, &input);
-//  resize2d_bilinear_assign_float(input);
-//
-//  Tensor output;
-//  aitisa_resize2d_bilinear(input, 5, 5, &output);
-//
-//  auto out_data = (float*)aitisa_tensor_data(output);
-//  int64_t size = aitisa_tensor_size(output);
-//  char path[] = "../../test/test_data/Resize_float2d_case1.dat";
-//  float test_data[25];
-//  read_date(path, test_data);
-//  for (int64_t i = 0; i < size; i++) {
-//    /* Due to the problem of precision, consider the two numbers
-//       are equal when their difference is less than 0.000001*/
-//    EXPECT_TRUE(abs(out_data[i] - test_data[i]) < 0.000001);
-//  }
-//
-//  aitisa_destroy(&input);
-//  aitisa_destroy(&output);
-//}
-//
-//TEST(Resize, Float2d_case2) {
-//  Tensor input;
-//  DataType dtype = kFloat;
-//  Device device = {DEVICE_CPU, 0};
-//  int64_t dims[2] = {5, 5};
-//  aitisa_create(dtype, device, dims, 2, NULL, 0, &input);
-//  resize2d_bilinear_assign_float(input);
-//
-//  Tensor output;
-//  aitisa_resize2d_bilinear(input, 3, 3, &output);
-//
-//  auto out_data = (float*)aitisa_tensor_data(output);
-//  char path[] = "../../test/test_data/Resize_float2d_case2.dat";
-//  float test_data[9];
-//  read_date(path, test_data);
-//  int64_t size = aitisa_tensor_size(output);
-//  for (int64_t i = 0; i < size; i++) {
-//    /* Due to the problem of precision, consider the two numbers
-//       are equal when their difference is less than 0.000001*/
-//    EXPECT_TRUE(abs(out_data[i] - test_data[i]) < 0.000001);
-//  }
-//
-//  aitisa_destroy(&input);
-//  aitisa_destroy(&output);
-//}
-//}  // namespace
-//}  // namespace aitisa_api
-int main(int argc, char **argv) {
-  std::cout << "resize" << std::endl;
-  return 0;
+#include "test_utlis.h"
+extern "C" {
+#include "src/nn/resize2d_bilinear.h"
+}
+
+int main(int argc, char** argv) {
+  if (argc == 3) {
+    Tensor input, output;
+    Device device = {DEVICE_CPU, 0};
+    int64_t input_ndim, result_ndim, input_dtype, result_dtype, input_num,
+        result_num;
+    std::vector<int64_t> input_dims_vector, result_dims_vector;
+    std::vector<float> input_data_vector, result_data_vector;
+    read_data(argv[1], &input_ndim, &input_dims_vector, &input_dtype,
+              &input_num, &input_data_vector);
+
+    int64_t* input_dims;
+    input_dims = (int64_t*)malloc(sizeof(int64_t) * input_ndim);
+    memset(input_dims, 0, sizeof(float) * input_ndim);
+    for (int i = 0; i < input_ndim; i++) {
+      input_dims[i] = input_dims_vector[i];
+    }
+
+    aitisa_create(aitisa_int_to_dtype(input_dtype), device, input_dims,
+                  input_ndim, nullptr, 0, &input);
+    assign(input, input_data_vector);
+    aitisa_resize2d_bilinear(input, 500, 500, &output);
+
+    auto output_data = (float*)aitisa_tensor_data(output);
+    read_data(argv[2], &result_ndim, &result_dims_vector, &result_dtype,
+              &result_num, &result_data_vector);
+
+    int64_t output_size = aitisa_tensor_size(output);
+    for (int i = 0; i < output_size; ++i) {
+      //      std::cout << output_data[i] << std::endl;
+      if (abs(output_data[i] - result_data_vector[i]) > 1e-3) {
+        std::cout << " mismatch" << std::endl;
+        exit(1);
+      }
+    }
+    free(input_dims);
+    aitisa_destroy(&input);
+    aitisa_destroy(&output);
+  } else if (argc > 3) {
+    std::cout << "Too many arguments supplied." << std::endl;
+    exit(1);
+  } else {
+    std::cout << "Two argument expected. Usage: ./resize2d_bilinear_test "
+                 "<input_path> <result_path> "
+              << std::endl;
+    exit(1);
+  }
 }
